69ded13affacde7c6f7f766e957b40495470a2b4
hotburnburn <13287686871@163.com>
2026-03-14 13:26:27 +0800

    add some xdg...

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,16 +1,23 @@
-# 过滤掉 WSL 默认注入的无效 games 路径
+# PATH相关，过滤games，添加.local/bin
 set PATH (string match -v '/usr/games' $PATH)
 set PATH (string match -v '/usr/local/games' $PATH)
 
 set -x PATH ~/.local/bin/ $PATH
 
+# XDG Base Directory
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+
+# rustup && cargo
+set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
+set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+if not contains "$CARGO_HOME/bin" $PATH
+    set -x PATH "$CARGO_HOME/bin" $PATH
+end
+
 # 如果 fnm 命令存在，则初始化
 if type -q fnm
     fnm env --use-on-cd | source
-end
-
-# rustup shell setup
-if not contains "$HOME/.cargo/bin" $PATH
-    # Prepending path in case a system-installed rustc needs to be overridden
-    set -x PATH "$HOME/.cargo/bin" $PATH
 end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,4 @@
 #     set -g fish_greeting ""
 # end
 
-# XDG Base Directory
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx XDG_STATE_HOME "$HOME/.local/state"
-
-abbr -a ins "sudo paru -S"
+abbr -a ins "paru -S"
