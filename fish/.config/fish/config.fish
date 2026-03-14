c9600249d3230b3bf3ff259538d0a029f0491295
hotburnburn <13287686871@163.com>
2026-03-14 12:53:18 +0800

    add xdg base dir

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,10 @@
 #     set -g fish_greeting ""
 # end
 
+# XDG Base Directory
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+
 abbr -a ins "sudo paru -S"
