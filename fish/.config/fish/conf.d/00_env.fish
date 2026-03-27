6589dc85e986d3ab92822b4afaeab1e6fc32cbf8
hotburnburn <13287686871@163.com>
2026-03-27 22:33:11 +0800

    embrace fish_add_path -g

--- fish/.config/fish/conf.d/00_env.fish
+++ fish/.config/fish/conf.d/00_env.fish
@@ -1,14 +1,7 @@
 # PATH相关，过滤games，添加.local/bin
 set PATH (string match -v '/usr/games' $PATH)
 set PATH (string match -v '/usr/local/games' $PATH)
-
-function myaddpath
-    if not contains $argv[1] $PATH
-        set -x PATH $argv[1] $PATH
-    end
-end
-
-myaddpath "$HOME/.local/bin"
+fish_add_path -g "$HOME/.local/bin"
 
 # XDG Base Directory
 set -gx XDG_CONFIG_HOME "$HOME/.config"
@@ -19,11 +12,11 @@ set -gx XDG_STATE_HOME "$HOME/.local/state"
 # rustup && cargo
 set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
-myaddpath "$CARGO_HOME/bin"
+fish_add_path -g "$CARGO_HOME/bin"
 
 # bun
 set -gx BUN_INSTALL "$HOME/.local/share/bun"
-myaddpath "$BUN_INSTALL/bin"
+fish_add_path -g "$BUN_INSTALL/bin"
 
 # fnm && npm
 if not set -q FNM_MULTISHELL_PATH
